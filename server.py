0a3441b8e2967b5ffd13e002599d27a5cd38cd21
Bao <nguyenkimbao.0708@gmail.com>
2024-10-10 20:53:40 +0700

    Initial commit

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from project import app
+from waitress import serve
 
 # Cấu hình Web Server
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=5003)
+	serve(app=app, host='127.0.0.1', port=5003)
